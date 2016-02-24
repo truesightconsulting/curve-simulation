f569952a235bbb3f8159112b3f082f44e4cd47ef
Xin Zhou <xinzhou1023@gmail.com>
2016-02-24 16:31:53 -0500

    support adstock formula

--- fit curves.R
+++ fit curves.R
@@ -1,5 +1,7 @@
 library(data.table);library(bit64)
-setwd("d:\\Archives\\Git\\opt-admin\\chase\\curve\\")
+setwd("c:\\Users\\XinZhou\\Documents\\GitHub\\kohls\\admin\\opt\\curve\\")
 learn.rate.start=1e-6 # the start point of learn rate for fitting curves (vary from curve file to curve file)
+for.opt=T # T if the fitting is for otptimization curves
+is.refit=F # refit with a/b curve or use the adstock formula
 
-source("d:\\Archives\\Git\\curve-simulation\\curve simulator from ad.R")
+source("c:\\Users\\XinZhou\\Documents\\GitHub\\curve-simulation\\curve simulator from ad.R")
